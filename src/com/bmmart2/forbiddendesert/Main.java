39d8a36b0d13de88ce6501ee6fc216c231e2fb04
Brandon Martinez <bmmart2@gmail.com>
2020-02-05 23:00:38 -0600

    testing

--- src/com/bmmart2/forbiddendesert/Main.java
+++ src/com/bmmart2/forbiddendesert/Main.java
@@ -9,7 +9,6 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-
                 System.out.println("Hello World");
                 Board b = new Board();
                 LinkedList<Integer> q = new LinkedList<Integer>();
